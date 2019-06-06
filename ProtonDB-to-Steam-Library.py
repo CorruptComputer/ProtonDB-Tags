9a3fe051341e3c55905616a58fd822efab3ce8f3
Nickolas Gupton <nickolas@gupton.xyz>
2019-06-06 18:57:25 -0500

    Initial commit

---
+++ ProtonDB-to-Steam-Library.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import os.path
+from os import path
+import vdf
+import urllib.request
+import json
+
+## Change this to match your system
+sharedconfig = "/home/[user]]/.steam/steam/userdata/[UserID]]/7/remote/sharedconfig.vdf"
+
+if (path.exists(sharedconfig) != True):
+    print("Please edit the top of this file to include your steam path!")
+    exit()
+
+data = vdf.load(open(sharedconfig))
+
+for appid in data["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["Apps"]:
+    try:
+        appid = int(appid)
+        protondb = json.load(urllib.request.urlopen("https://www.protondb.com/api/v1/reports/summaries/" + str(appid) + ".json"))["trendingTier"]
+
+        print(str(appid) + " " + protondb)
+
+        data["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["Apps"][str(appid)]["tags"] = vdf.VDFDict()
+        data["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["Apps"][str(appid)]["tags"]["0"] = protondb
+        vdf.dump(data, open(sharedconfig,'w'), pretty=True)
+
+    except ValueError:
+        continue
+    
+    except urllib.error.HTTPError:
+        continue
+
+vdf.dump(data, open(sharedconfig,'w'), pretty=True)
---
+++ README.md
@@ -0,0 +1,19 @@
+# ProtonDB to Steam Library
+
+This is just a small python script I made to pull ratings from ProtonDB and import them into your Steam library as tags. 
+
+### Running
+
+**WARNING:** This will clear all of your current tags in Steam. You have been warned!
+
+You'll need to edit the file and add your Steam install path and UserID to here:
+
+```python
+sharedconfig = "/home/[user]/.steam/steam/userdata/[userID]/7/remote/sharedconfig.vdf"
+```
+
+Once that is set you can just run with: `python ProtonDB-to-Steam-Library.py`
+
+### Made with:
+
+https://github.com/ValvePython/vdf
